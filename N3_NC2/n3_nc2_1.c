4b2e99ecbcea9344b4a3e93fbea7cf94c2a2ca1a
aurycruz <aurycruz1989@gmail.com>
2025-09-18 22:03:19 +0000

    n3_nc2_1.c

---
+++ N3_NC2/n3_nc2_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 20;
+
+    printf("a > b: %d\n", a > b);
+    printf("a < b: %d\n", a < b);
+    printf("a == b: %d\n", a == b);
+    printf("a !n b: %d\n", a !n b);
+
+
+    return 0;
+
+
+
+
+
+
+
+}
